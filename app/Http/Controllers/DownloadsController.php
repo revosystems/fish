55a5291b4c564a194d8f82aa7dcdd5d4f79cbcd9
Pau Benet Prat <pau@revo.works>
2019-05-09 10:30:14 +0200

    move dossiers together

--- app/Http/Controllers/DownloadsController.php
+++ app/Http/Controllers/DownloadsController.php
@@ -6,13 +6,13 @@ class DownloadsController extends Controller
 {
     public function download($folder, $file_name)
     {
-        $file_path = public_path('docs/'.$folder.'/'.$file_name);
+        $file_path = public_path("docs/{$folder}/{$file_name}");
         return response()->download($file_path);
     }
 
     public function downloadDossier($file_name)
     {
-        $file_path = public_path('docs/dossiers/' . auth()->user()->getOrganizationName() . '/'.$file_name);
+        $file_path = public_path("docs/dossiers/{$file_name}");
         return response()->download($file_path);
     }
 }
