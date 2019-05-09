3d062cb95bab8438d79e0b418d65053318bf16a0
Pau Benet Prat <pau@revo.works>
2019-05-09 10:26:38 +0200

    remove config get platform

--- app/Http/Controllers/DownloadsController.php
+++ app/Http/Controllers/DownloadsController.php
@@ -2,17 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class DownloadsController extends Controller
 {
-    public function download($folder,$file_name) {
+    public function download($folder, $file_name)
+    {
         $file_path = public_path('docs/'.$folder.'/'.$file_name);
         return response()->download($file_path);
     }
 
-    public function downloadDossier($file_name) {
-        $file_path = public_path('docs/dossiers/'.config('app.platform').'/'.$file_name);
+    public function downloadDossier($file_name)
+    {
+        $file_path = public_path('docs/dossiers/' . auth()->user()->getOrganizationName() . '/'.$file_name);
         return response()->download($file_path);
     }
 }
--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include("layouts.partials.header.head")
-    {{--    <body class="auth {{ auth()->user()->getOrganizationName() }}">--}}
     <body class="auth">
         @include("layouts.partials.loaders.preloader")
         <div class="container login-container">
