8c3b12390d918d9f1192c18b649bb2fbb8d00a41
Pau Benet Prat <pau@revo.works>
2019-05-13 12:24:06 +0200

    Display organizations properly

--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -31,4 +31,21 @@ class Organization extends Model
     {
         return $this->hasManyThrough(Lead::class, Organization::class);
     }
+
+    public function getParentOrganizations($organizations = [])
+    {
+        return $this->organization_id ? $this->parentOrganization->getParentOrganizations($organizations)->push($this->parentOrganization) : collect($organizations);
+    }
+
+    public function getChildrenOrganizations()
+    {
+        if (! $this->organizations->count()) {
+            return collect();
+        }
+        $organizationsArray = $this->organizations;
+        return $this->organizations->each(function ($organization) use ($organizationsArray) {
+            $organizationsArray->push($organization->getChildrenOrganizations()->flatten());
+        })->flatten();
+        return $organizationsArray;
+    }
 }
